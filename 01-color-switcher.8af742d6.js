047b5b2fbb97d90dab85a8018fbf9c7444e7df0d
piterbar <piotr.baraniak@gmail.com>
2022-11-02 10:40:51 +0000

    Deploying to gh-pages from @ piterbar/goit-js-hw-09@9e2e33d2f22fe9d869895113651478cf271668ca 🚀

---
+++ 01-color-switcher.8af742d6.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){timerId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.8af742d6.js.map
